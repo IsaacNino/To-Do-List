9b2bec24b0bbe488f92498a149c9b29a6fb3090d
Isaac Nino <isaacdavidnino@gmail.com>
2023-02-03 16:17:40 -0600

    brought my code in from the replit after translating from JS
    changed /scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,32 @@
+//IIFE wrapper keeps code from accessing the gloabal state
+function newItem(){
 
+    //code for adding new item to the list
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
 
+    //how to browswer notifies the user not to leave the input empty
+    if (inputValue === '') {
+        alert("You must add something!");
+    } else {
+        $('#list').append(li);
+    }
+    
+    //how items are crossed out from the list
+    li.on("dblclick", function crossOut() {
+        li.toggleClass("strike");
+    });
+
+    //how items are deleted from the list
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
+
+        crossOutButton.on("click", deleteListItem);
+        function deleteListItem(){
+            li.addClass("delete")
+        }
+    $('#list').sortable();
+    //
+}
